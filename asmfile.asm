fa893e73e2d087556897cee076989ff5443d6008
JanicePolito <jacekkuzyn87@gmail.com>
2025-03-12 22:23:56 +0100

    cleanup

--- asmfile.asm
+++ asmfile.asm
@@ -10,10 +10,6 @@ myString		BYTE "ABCD", 0
 .code
 mboxtest PROC
 
-	;sub rsp, 28h   ;reserved the stack area as parameter passing area
-	;mov rcx, 12345678   ; specify Exit Code
-	;call ExitProcess
-
 	sub rsp, 28h		; reserved shadow area
 
 	mov rcx, 0
