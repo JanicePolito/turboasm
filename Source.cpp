30eb774cdc5a1af90ea3b5ce7773d86401fc5f78
JanicePolito <jacekkuzyn87@gmail.com>
2025-03-12 20:42:11 +0100

    few new PROCs

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,22 @@
 extern "C" void mboxtest(); // Declare the PROC
 extern "C" int vret(); // Declare the PROC
+extern "C" const char* lowerCase(); // Declare the PROC
+extern "C" const char* lowerCaseWithArg(char* str); // Declare the PROC
 
 #include <iostream>
 
 void main()
 {
 	std::cout << "Hello, World from standard c++ code!" << std::endl;
+
 	int result = vret();
 	std::cout << "The result from asm file: " << result << std::endl;
+
+	const char* lowerCaseStr = lowerCase();
+	std::cout << "convertion to lower case from asm file: " << lowerCaseStr << std::endl;
+
+	char myStr[] = "EFGH";
+	std::cout << "convertion to lower case from asm file with passing the arguments: " << lowerCaseWithArg(myStr) << std::endl;
+
 	mboxtest();
 }
--- asmfile.asm
+++ asmfile.asm
@@ -5,6 +5,7 @@ MessageBoxA PROTO
 myText			db "Hello World from asm file!", 0
 myCaption		db "Message Box Testing", 0
 myValue			QWORD 1234
+myString		BYTE "ABCD", 0
 
 .code
 mboxtest PROC
@@ -32,5 +33,40 @@ vret PROC
     mov rax, 42    ; Place return value (42) in RAX
     ret            ; Return to the caller
 vret ENDP
+
+lowerCase PROC
+
+	add [myString], 20h
+	add [myString+1], 20h
+	add [myString+2], 20h
+	add [myString+3], 20h
+	lea rax, myString
+	ret
+
+lowerCase ENDP
+
+lowerCaseWithArg PROC
+	; RCX contains the address of the string passed from C++
+    mov rax, rcx        ; Save the input string address in RAX
+
+	mov dl, [rcx]	; Load the current character into DL
+	add dl, 20h		; Convert to lowercase by adding 32 (difference between 'A' and 'a')
+	mov [rcx], dl	; Store the lowercase character back
+	inc rcx			; Move to the next character
+	mov dl, [rcx]
+	add dl, 20h
+	mov [rcx], dl
+	inc rcx
+	mov dl, [rcx]
+	add dl, 20h
+	mov [rcx], dl
+	inc rcx
+	mov dl, [rcx]
+	add dl, 20h
+	mov [rcx], dl
+
+	ret				; Return the modified string (pointer is still in RAX)
+
+lowerCaseWithArg ENDP
 
 END
